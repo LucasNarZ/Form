6c79b5173b25250334e727c3e04ef58688de483e
LucasNarZ <lucasnarlochz@gmail.com>
2023-07-04 18:03:04 -0300

    .

---
+++ form/src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css';
+
+export default function AboutUs(){
+    return (
+        <div className="min-w-screen min-h-screen">
+            <p className='p-20'>Lucasasdasdadasdasdasdasdasdasda</p>
+        </div>
+    )
+}
