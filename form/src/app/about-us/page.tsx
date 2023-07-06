8149436fe51038340f2d70a56a95be311471c02d
Lucas <maritsanarloch@gmail.com>
2023-07-06 20:54:10 -0300

    meta tags

--- form/src/app/about-us/page.tsx
+++
@@ -1,9 +0,0 @@
-import 'tailwindcss/tailwind.css';
-
-export default function AboutUs(){
-    return (
-        <div className="min-w-screen min-h-screen">
-            <p className='p-20'>Lucasasdasdadasdasdasdasdasdasda</p>
-        </div>
-    )
-}
--- form/src/app/page.tsx
+++ form/src/app/page.tsx
@@ -3,31 +3,45 @@
 import 'tailwindcss/tailwind.css';
 import { Container, TextField, Typography, Button, Box } from "@mui/material"
 
+import React from 'react';
+
+import Head from 'next/head'
 import { FormInput } from './components/input'
 import { AddInput } from './components/addinput'
 
-export default function Home():JSX.Element {
-    return (
-        <Box className="flex items-center w-screen bg-backgroundImage w-formHeight bg-center bg-clip-content bg-no-repeat bg-cover content-'' before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0  before:bg-filter before:mix-blend-multiply">
-            <Container className='bg-primary z-10 rounded-xl flex flex-col items-center relative ' maxWidth="sm">
-                <h3 className='mt-5 font-xl'>Dev Form</h3>
-                <FormInput label="Name" validation={/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/} />
-                <FormInput label="Email" validation={/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/} />
-                <FormInput label="Phone" validation={/^[0-9]{7}$/} />
-                <FormInput label="Years of experience" validation={/^[0-9]+$/}/>
-                <FormInput label="Field" type='select' options={["Front-End", "Back-End", "Mobile", "Other"]} />
-                <FormInput label="GitHub Profile Link" validation={/^github\.com\/[a-zA-Z0-9._%+-]+$/} />
-                <FormInput label="Linkedin Profile Link" validation={/^github\.com\/[a-zA-Z0-9._%+-]+$/} />
-                <AddInput label="Skills(max 3)" />
-                <AddInput label="Projects Link(max 3)" />
-                <Button variant='outlined' className="z-20"sx={{
-                    position:"absolute",
-                    bottom:"20px",
-                    left:"50%",
-                    transform:"translateX(-50%)",
-                    width:"200px",
-                }}>Send</Button>
-            </Container>
-        </Box>
+export default function Home(){
+    return(
+        <React.Fragment>
+            <Head>
+                <meta charSet="UTF-8"/>
+                <link rel="icon" href="icon.svg" type="image/svg+xml"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta name="keywords" content="dev developers form front-end search"/>
+                <meta name="title" content='Developers Form'/>
+                <meta name="description" content='Form for developers and a dashboard for data.'/>
+            </Head>
+            <Box className="flex items-center bg-backgroundImage w-formHeight bg-center bg-clip-content bg-no-repeat bg-cover content-'' before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0  before:bg-filter before:mix-blend-multiply">
+                <Container className='bg-primary z-10 rounded-xl flex flex-col items-center relative ' maxWidth="sm">
+                    <h3 className='mt-5 font-xl'>Dev Form</h3>
+                    <FormInput label="Name" validation={/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/} />
+                    <FormInput label="Email" validation={/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/} />
+                    <FormInput label="Phone" validation={/^[0-9]{7}$/} />
+                    <FormInput label="Years of experience" validation={/^[0-9]+$/}/>
+                    <FormInput label="Field" type='select' options={["Front-End", "Back-End", "Mobile", "Other"]} />
+                    <FormInput label="GitHub Profile Link" validation={/^github\.com\/[a-zA-Z0-9._%+-]+$/} />
+                    <FormInput label="Linkedin Profile Link" validation={/^github\.com\/[a-zA-Z0-9._%+-]+$/} />
+                    <AddInput label="Skills(max 3)" />
+                    <AddInput label="Projects Link(max 3)" />
+                    <Button variant='outlined' className="z-20"sx={{
+                        position:"absolute",
+                        bottom:"20px",
+                        left:"50%",
+                        transform:"translateX(-50%)",
+                        width:"200px",
+                    }}>Send</Button>
+                </Container>
+            </Box>
+        </React.Fragment>
+                 
     )
 }
