67f90154b90e4469948e117dedcd45b6ccd89ef8
LucasNarZ <lucasnarlochz@gmail.com>
2023-08-13 12:32:29 -0300

    migrating from material to daisy

--- form/src/app/page.tsx
+++ form/src/app/page.tsx
@@ -1,16 +1,10 @@
 'use client';
 
-import React from 'react';
-import 'tailwindcss/tailwind.css';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-
-import { Container, Button, Box, TextField } from "@mui/material";
 import { useForm } from 'react-hook-form';
 
-
-import { FormInput } from './components/input';
-import { AddInput } from './components/addinput';
+import { FormInput } from './componentes/input';
+import { AddInput } from './componentes/addinput';
 
 export default function Home(){
     const { handleSubmit, register, formState:{errors} } = useForm();
@@ -22,42 +16,50 @@ export default function Home(){
     }
 
     return(
-        <React.Fragment>
-            <Box className="flex items-center bg-backgroundImage min-h-formHeight w-screen bg-center bg-clip-content bg-no-repeat bg-cover content-'' before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0  before:bg-filter before:mix-blend-multiply">
-                <Container className='bg-primary z-10 rounded-xl flex flex-col pb-20 items-center relative ' maxWidth="sm">
-                    <h3 className='my-8 text-4xl text-center'>Dev Form</h3>
-                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center w-full'>
-                        <TextField id="outlined-basic" label={"Name"} variant='outlined'  error={!!errors.name} className="w-inputWidth mt-3 mb-3"
-                        {...register("name", {required:true, pattern:/^[a-zA-Z]+$/})}  />
-
-                        <TextField id="outlined-basic" label={"Email"} variant='outlined'  error={!!errors.email} className="w-inputWidth my-3"
-                        {...register("email", {required:true, pattern:/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/})}  />
-
-                        <TextField id="outlined-basic" label={"Phone"} variant='outlined'  error={!!errors.phone} className="w-inputWidth my-3"
-                        {...register("phone", {required:true, pattern:/^[0-9]{7}$/})} />
-
-                        <TextField id="outlined-basic" label={"Years of Experience"} variant='outlined'  error={!!errors.experience} className="w-inputWidth my-3" {...register("experience", {required:true, pattern:/^[0-9]+$/})} /> 
-
-                        <FormInput label="Field" options={["Front-End", "Back-End", "Mobile", "Other"]}/>
-
-                        <TextField id="outlined-basic" label={"Git Hub Profile Link"} variant='outlined'  error={!!errors.github} className="w-inputWidth my-3" {...register("github", {required:true, pattern:/^https:\/\/github\.com\/[a-zA-Z0-9._%+-]+$/})} /> 
-
-                        <TextField id="outlined-basic" label={"Linkedin Profile Link"} variant='outlined'  error={!!errors.linkedin} className="w-inputWidth my-3" {...register("linkedin", {required:true, pattern:/^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9._%+-\/]+$/})} /> 
-
-                        <AddInput label="Skills(max 3)" />
-                        <AddInput label="Projects Link(max 3)" />
-                        <Button variant='outlined' className="z-20"  type="submit" sx={{
-                        position:"absolute",
-                        bottom:"20px",
-                        left:"50%",
-                        transform:"translateX(-50%)",
-                        width:"200px",
-                        }}>Send</Button>
-                    </form>
+        <div className="flex items-center justify-center bg-backgroundImage min-h-[100vh] w-screen bg-center bg-clip-content bg-no-repeat bg-cover content-'' before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0  before:bg-filter before:mix-blend-multiply">
+            <div className='bg-primary z-10 rounded-xl flex flex-col pb-20 items-center relative max-w-[600px] w-[80%]'>
+                <h3 className='my-8 text-4xl text-center'>Dev Form</h3>
+                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center w-full'>
+                    <div className='w-[80%]'>
+                        <label>Name</label> <br />
+                        <input className={`input w-full my-3 bg-white border-black ${!!errors.name ? "input-error" : ""}`} {...register("name", {required:true, pattern:/^[a-zA-Z]+$/})}  />
+                    </div>
                     
-                </Container>
-            </Box>
-        </React.Fragment>
-                 
+                    <div className='w-[80%]'>
+                        <label>Email</label> <br />
+                        <input className={`input w-full my-3 bg-white border-black ${!!errors.email ? "input-error" : ""}`} {...register("email", {required:true, pattern:/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/})}  />
+                    </div>
+
+                    <div className='w-[80%]'>
+                        <label>Phone</label> <br />
+                        <input className={`input w-full my-3 bg-white border-black ${!!errors.phone ? "input-error" : ""}`} {...register("phone", {required:true, pattern:/^[0-9]{7}$/})} />
+                    </div>
+
+                    <div className='w-[80%]'>
+                        <label>Years of Experience</label> <br />
+                        <input className={`input w-full my-3 bg-white border-black ${!!errors.experience ? "input-error" : ""}`} {...register("experience", {required:true, pattern:/^[0-9]+$/})} /> 
+                    </div>
+
+                    <FormInput label="Field" options={["Front-End", "Back-End", "Mobile", "Other"]}/>
+
+                    <div className='w-[80%]'>
+                        <label>GitHub Profile Link</label> <br />
+                        <input className={`input w-full my-3 bg-white border-black ${!!errors.github ? "input-error" : ""}`} {...register("github", {required:true, pattern:/^https:\/\/github\.com\/[a-zA-Z0-9._%+-]+$/})} /> 
+                    </div>
+
+                    <div className='w-[80%]'>
+                        <label>linkedin Profile Link</label> <br />
+                        <input className={`input w-full my-3 bg-white border-black ${!!errors.linkedin ? "input-error" : ""}`} {...register("linkedin", {required:true, pattern:/^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9._%+-\/]+$/})} /> 
+                    </div>
+
+
+
+                    <AddInput label="Skills(max 3)" />
+                    <AddInput label="Projects Link(max 3)" />
+                    <button className="btn z-20 absolute bottom-[20px] right-[50%] translate-x-2/4 w-[200px]"  type="submit">Send</button>
+                </form>
+                
+            </div>
+        </div>             
     )
 }
