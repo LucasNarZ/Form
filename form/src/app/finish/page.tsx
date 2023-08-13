d6018523123e16d55b43b94c980787e44b63e7e0
LucasNarZ <lucasnarlochz@gmail.com>
2023-08-13 12:31:50 -0300

    finishing the finish section

--- form/src/app/finish/page.tsx
+++ form/src/app/finish/page.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 
+
+
 export default function Finish(){
     return(
-        <React.Fragment>
-            a
-        </React.Fragment>
+        <div className="bg-white w-screen h-screen flex items-center justify-center">
+            <h1 className="text-center text-4xl">Thank you for your response</h1>
+        </div>
     )
 }
